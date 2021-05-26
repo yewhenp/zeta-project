bc8ec7c1f96d3ef42163d69b718c81b1affbb329
Yevhen Pankevych <y.pankevych@ucu.edu.ua>
2021-05-26 17:17:08 +0300

    added pagination

--- src/modules/MainPage/components/MainContent/index.jsx
+++ src/modules/MainPage/components/MainContent/index.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux'
 import { Divider } from '@material-ui/core'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import Pagination from '@material-ui/lab/Pagination'
+
 import PostOverview from '../PostOverview'
 import useStyles from './styles'
 
@@ -14,14 +16,39 @@ const MainPage = () => {
   const selectedValues = useSelector(state => state.selectedValues)
 
   const [postData, updatePostData] = useState([])
-  const getPostData = async () => {
-    const resp = await fetch(`${BASE_API}/posts/1?many=true&from=0&to=10`)
+  const getPostData = async (from, to) => {
+    const resp = await fetch(
+      `${BASE_API}/posts/1?many=true&from=${from}&to=${to}`,
+    )
     let data = await resp.json()
     data = data.response
     updatePostData(data)
   }
+
+  const perPage = 5
+  const [pageCount, updatePageCount] = useState(1)
+  // eslint-disable-next-line no-unused-vars
+  const getPageCount = async () => {
+    const resp = await fetch(`${BASE_API}/posts/1?count=1`)
+    let data = await resp.json()
+    data = data.response
+    let tempPageCount = Math.floor(data / perPage)
+    const tempPageCountLeft = data % perPage
+    if (tempPageCountLeft !== 0) {
+      tempPageCount += 1
+    }
+    updatePageCount(tempPageCount)
+  }
+
+  const [page, updatePage] = useState(1)
+  const setPage = val => {
+    updatePage(val)
+    getPostData(val * pageCount, (val + 1) * pageCount)
+  }
+
   useEffect(() => {
-    getPostData()
+    // getPageCount()
+    getPostData(0, perPage)
   }, [])
 
   const includedInSelected = tags => {
@@ -66,6 +93,12 @@ const MainPage = () => {
             ),
         )}
       </List>
+      <Pagination
+        count={pageCount}
+        page={page}
+        onChange={(event, val) => setPage(val)}
+        color="primary"
+      />
     </div>
   )
 }
